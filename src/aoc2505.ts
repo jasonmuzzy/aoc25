f59d6d34588d8e617a2ee37b0e971269cff26f77
Jason Muzzy <15488120+jasonmuzzy@users.noreply.github.com>
2025-12-06 01:51:51 +0000

    Improve day 5 part 2

--- src/aoc2505.ts
+++ src/aoc2505.ts
@@ -1,8 +1,5 @@
 import { run } from 'aoc-copilot';
 
-// The one where we're counting fresh ingredients that appear in a range
-// And in part 2 we have to give the total count of all possible fresh ingredients
-
 async function solve(
     inputs: string[], // Contents of the example or actual inputs
     part: number,     // Indicates whether the solver is being called for part 1 or 2 of the puzzle
@@ -11,32 +8,29 @@ async function solve(
 ): Promise<number | bigint | string> {
     let answer: number | bigint | string = 0;
 
-    // Parse the input:
-    // First there's a list of ranges like 1-10
-    // Then there's a blank row
-    // Followed by a list of single ingredient numbers
+    // Parse the input, splitting on the blank line into two sections: 1) ranges, and 2) ingredients
     const [ranges, ingredients] = inputs.join('\n').split('\n\n').map(row => row.split('\n').map(range => range.includes('-') ? range.split('-').map(Number) : parseInt(range))) as [[number, number][], number[]];
+
+    // Part 1: Count the ingredients that occur in one of the fresh ingredient ranges
     if (part === 1) {
         answer = ingredients.reduce((totalFresh, ingredient) => totalFresh += (ranges.some(([low, high]) => low <= ingredient && high >= ingredient) ? 1 : 0), 0);
+
+    // Part 2: Count the total possible ingredients from all ranges
+    // Note: Attempting to add all ingredient numbers to a set to get a distinct count causes OOM
     } else {
-        // We can't just make a set of unique ingredient numbers because that causes an OOM error.
-        // Instead we have to merge all of the fresh ingredient ranges and then count the number of ingredients in the resulting ranges.
-        const mergedRanges = ranges.reduce((mergedRanges, [newLow, newHigh]) => {
-            const overlaps = [...mergedRanges.entries()].filter(([i, [low, high]]) => low <= newHigh && high >= newLow);
-            if (overlaps.length === 0) { // New, non-overlapping range
-                mergedRanges.push([newLow, newHigh]);
-            } else if (overlaps.length === 1) { // Extend an existing range
-                const [i, [low, high]] = overlaps[0];
-                mergedRanges[i] = [Math.min(low, newLow), Math.max(high, newHigh)];
-            } else { // Combine two existing ranges, potentially extending one or both of them
-                const [firstLow, firstHigh] = mergedRanges[overlaps[0][0]];
-                const [secondLow, secondHigh] = mergedRanges[overlaps[1][0]];
-                mergedRanges[overlaps[0][0]] = [Math.min(newLow, firstLow, secondLow), Math.max(newHigh, firstHigh, secondHigh)];
-                mergedRanges.splice(overlaps[1][0], 1);
+        answer = ranges
+            .toSorted(([a], [b]) => a - b) // Sort first so we can easily merge ranges
+            .map(r => [...r, r[1] - r[0] + 1]) // Add a 3rd element which is the ingredient counter
+            .reduce((agg, [newLow, newHigh]) => {
+                let [prevLow, prevHigh, count] = agg;
+                if (newLow <= prevHigh) { // The new range extends the previous one
+                    agg = [prevLow, Math.max(prevHigh, newHigh), count + Math.max(0, newHigh - prevHigh)];
+                } else { // The new range is discrete from the previous one
+                    agg = [newLow, newHigh, count + newHigh - newLow + 1];
+                }
+                return agg;
             }
-            return mergedRanges;
-        }, [] as [number, number][]);
-        answer = mergedRanges.reduce((count, [low, high]) => count += high - low + 1, 0); // Count the ingredients in the merged ranges
+            )[2];
     }
 
     return answer;
