66d657c998b7580b44eca9e8b52ca558a9e0ef94
jasonmuzzy <15488120+jasonmuzzy@users.noreply.github.com>
2025-12-17 21:22:24 -0800

    Day 1: change to modulo

--- src/aoc2501.ts
+++ src/aoc2501.ts
@@ -1,39 +1,33 @@
 import { run } from 'aoc-copilot';
 
-async function solve(
-    inputs: string[], // Contents of the example or actual inputs
-    part: number,     // Indicates whether the solver is being called for part 1 or 2 of the puzzle
-    test: boolean,    // Indicates whether the solver is being run for an example or actual input
-    additionalInfo?: { [key: string]: string } // Additional info for some puzzles with multiple examples
-): Promise<number | bigint | string> {
-    let answer: number | bigint | string = 0;
+async function solve(inputs: string[], part: number) {
 
+    // Get the door password by following L/R turn instructions for a safe dial and counting the number
+    // of times we land on (or pass in part 2) zero
+    const mod = (n: number, d: number) => ((n % d) + d) % d; // Modulo for turning left past 0
+    let zeroCount = 0;
     let dial = 50;
     for (const rotation of inputs) {
+        const direction = rotation.substring(0, 1);
         const clicks = parseInt(rotation.substring(1));
-        for (let click = 1; click <= clicks; click++) {
-            if (rotation.startsWith('R')) {
-                dial++;
-            } else {
-                dial--;
-            }
-
-            if (dial === 100) {
-                dial = 0;
-            } else if (dial === -1) {
-                dial = 99;
-            }
-            
-            if (part === 2 && dial === 0) {
-                answer++;
-            }
-        }
-        if (part === 1 && dial === 0) {
-            answer++;
+        if (part === 1) { // Count times we land on zero
+            dial = mod(dial + (direction === 'R' ? clicks : -clicks), 100);
+            zeroCount += dial === 0 ? 1 : 0;
+        } else { // Count times we land on or pass zero
+            const oldDial = dial;
+            const fullRotations = Math.floor(clicks / 100); // Full rotations pass zero
+            const newDial = mod(dial + (direction === 'R' ? clicks : -clicks), 100);
+            const partialRot =
+                newDial === 0 || (
+                    (direction === 'R' && newDial < dial) ||
+                    (direction === 'L' && newDial > dial && oldDial !== 0) // Turning left from zero doesn't pass zero again
+                ) ? 1 : 0;
+            dial = newDial;
+            zeroCount += fullRotations + partialRot;
         }
     }
 
-    return answer;
+    return zeroCount;
 }
 
 run(__filename, solve);
